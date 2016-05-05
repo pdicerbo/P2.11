847d16a7003dc969f950eeffcfc654312fa5aff7
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-05-05 17:45:19 +0200

    safe commit

--- Day2/KMeans.py
+++ Day2/KMeans.py
@@ -8,24 +8,37 @@ def main():
     data = np.loadtxt("s3.txt")
 
     k = 15
+    NIter = 1
+    
     npoints = data.shape[0]
+    ObjFunc  = np.zeros(NIter)
+    MyAssign = np.zeros((NIter, npoints))
 
-    MyK = KMeans(k,npoints)
+    for NIt in range(NIter):
+        print("\n\tStarting iteration ", NIt+1, " of ", NIter)
 
-    MyK.init_system(data)
-    MyK.clusterize()
-    # print(MyK.centers)
+        MyK = KMeans(k,npoints)
 
+        MyK.init_system(data)
+        MyK.clusterize()
+        ObjFunc[NIt] = MyK.objective_func()
+        MyAssign[NIt,:] = np.copy(MyK.membership)
+        
+        # h = np.zeros(k, dtype = float)
+        # # ind = 0
+        # # for p in pdef:
+        # #     h[ind] = ((p) / (pmax-pmin))*250.
+        # #     ind += 1
+        # h = [l/(k+1) for l in range(k+1)]
+        # cdef = [colorsys.hsv_to_rgb(x/360., 1., 1.) for x in h]
+        # h = np.linspace(0, 1, k+2)
 
-    # h = np.zeros(k, dtype = float)
-    # # ind = 0
-    # # for p in pdef:
-    # #     h[ind] = ((p) / (pmax-pmin))*250.
-    # #     ind += 1
-    # h = [l/(k+1) for l in range(k+1)]
-    # cdef = [colorsys.hsv_to_rgb(x/360., 1., 1.) for x in h]
-    # h = np.linspace(0, 1, k+2)
+        print("\tNumber of iteration: ", MyK.n_iter)
+        print("\tObjectiveFuncion: ", ObjFunc[NIt])
 
+    print("\tObjective Function minimum: ", ObjFunc.min())
+    print("\tPlotting...")
+    MinIndex = np.argmin(ObjFunc)
     plt.figure()
 
     for j in range(MyK.MyData.shape[0]):
@@ -47,21 +60,21 @@ def main():
         if MyK.membership[j] == 6:
             plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'y.')
         if MyK.membership[j] == 7:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'r.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'r*')
         if MyK.membership[j] == 8:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'b.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'b*')
         if MyK.membership[j] == 9:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'g.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'g*')
         if MyK.membership[j] == 10:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'k.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'k*')
         if MyK.membership[j] == 11:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'c.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'c*')
         if MyK.membership[j] == 12:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'm.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'm*')
         if MyK.membership[j] == 13:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'y.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'y*')
         if MyK.membership[j] == 14:
-            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'k.')
+            plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], 'ko')
 
         # s = str(h[1+MyK.membership[j]])
         # plt.plot(MyK.MyData[j,0], MyK.MyData[j,1], color = h[1+MyK.membership[j]])
